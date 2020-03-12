446ab8c6dfd27240b737e8990ea6198ab45bd4f1
leyiqiang <leyiqiang@gmail.com>
2020-03-12 21:48:12 +0800

    add pics

---
+++ pages/about/about.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class='page'>
+		<view class='body'>
+		<p>图言图语是由易知健康开发的一款帮助特殊儿童交流的应用, 
+		如果你对图言图语有任何建议, 或者你也想和我们一起陪伴可爱的孩子们，那就快来聊聊吧！</p>
+		<p>我们的官网: easohealth.com</p>
+		<p>联系电话: 19925414371</p>
+		<p>联系邮箱: winnie@easohealth.com</p>
+		<!-- <img src='../../static/icons/easologo.png'> -->
+		</view>
+		<!-- <official-account>s</official-account> -->
+
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	@import url("about.css");
+	.page {
+		width: 100%;
+		height: 100%;
+		background-color: #f7f7f7;
+		position: absolute;
+	}
+</style>
